b83457e08b8a0de1c0373adcc3cdda9f699244fd
Maxim Tschumak <maxim.tschumak@gmail.com>
2014-12-04 12:35:44 +0100

    created initial project structure

---
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/App.java
@@ -0,0 +1,12 @@
+package de.tuberlin.cit;
+
+/**
+ * Main Class a.k.a. Entry Point.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
