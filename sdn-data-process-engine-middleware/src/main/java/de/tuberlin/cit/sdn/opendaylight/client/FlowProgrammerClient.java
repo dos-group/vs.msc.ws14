892c7c82db7907d355ff20d8956d35ae5959dcc4
Robert Oberbeck <rob-oberbeck@users.noreply.github.com>
2014-12-22 13:39:43 +0100

    added put and delete FlowConfig

--- sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/opendaylight/client/FlowProgrammerClient.java
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/opendaylight/client/FlowProgrammerClient.java
@@ -1,6 +1,9 @@
 package de.tuberlin.cit.sdn.opendaylight.client;
 
+import com.sun.jersey.api.client.WebResource;
+import de.tuberlin.cit.sdn.opendaylight.model.flow.FlowConfig;
 import de.tuberlin.cit.sdn.opendaylight.model.flow.Flows;
+import de.tuberlin.cit.sdn.opendaylight.model.node.Node;
 
 public class FlowProgrammerClient extends AbstractClient {
 
@@ -16,5 +19,26 @@ public class FlowProgrammerClient extends AbstractClient {
     public Flows getFlows(String container) {
         Flows flows = resource(container).get(Flows.class);
         return flows;
+    }
+
+    public void putFlowConfig(FlowConfig flowConfig) {
+        putFlowConfig(flowConfig, "default");
+    }
+
+    public void putFlowConfig(FlowConfig flowConfig, String container) {
+        flowConfigResource(flowConfig, container).put(flowConfig);
+    }
+
+    public void deleteFlowConfig(FlowConfig flowConfig) {
+        deleteFlowConfig(flowConfig, "default");
+    }
+
+    public void deleteFlowConfig(FlowConfig flowConfig, String container) {
+        flowConfigResource(flowConfig, container).delete();
+    }
+
+    private WebResource.Builder flowConfigResource(FlowConfig flowConfig, String container) {
+        Node node = flowConfig.node;
+        return resource(container + "/node/" + node.type + "/" + node.id + "/staticFlow/" + flowConfig.name);
     }
 }
