fb6a5dc27c2cf8b9a1210aeb89424dface96ce32
Nico Scherer <jdam@live.de>
2015-01-03 16:59:18 +0100

    unused import

--- sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/Utils.java
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/Utils.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
 
 /**
  * Created by Nico on 02.01.2015.
