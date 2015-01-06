6f7e2deff4999f0737470a1d52f472755a128494
Robert Oberbeck <rob-oberbeck@users.noreply.github.com>
2015-01-06 22:22:17 +0100

    added missing fields to FlowConfig

--- sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/opendaylight/model/flow/FlowConfig.java
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/sdn/opendaylight/model/flow/FlowConfig.java
@@ -5,12 +5,31 @@ import de.tuberlin.cit.sdn.opendaylight.model.node.Node;
 import java.util.List;
 
 public class FlowConfig {
-    public boolean installInHw;
     public String name;
     public Node node;
     public Integer ingressPort;
     public Integer priority;
+    public Integer hardTimeout;
+    public Integer idleTimeout;
+    public String cookie;
+
+    // Layer 2
     public String etherType;
+    public Integer vlanId;
+    public Integer vlanPriority;
+    public String dlSrc;
+    public String dlDst;
+
+    // Layer 3
     public String nwSrc;
+    public String nwDst;
+    public Integer tosBits;
+
+    // Layer 4
+    public Integer tpSrc;
+    public Integer tpDst;
+    public String protocol;
+
+    public boolean installInHw;
     public List<String> actions;
 }
