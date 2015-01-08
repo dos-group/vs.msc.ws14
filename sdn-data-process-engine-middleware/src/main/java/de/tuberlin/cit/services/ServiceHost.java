26897245c8c3a0756e1e97e587b3c6f761f95d86
Nico Scherer <jdam@live.de>
2015-01-08 13:04:38 +0100

    added basic execution host service

--- sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/services/ServiceHost.java
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/services/ServiceHost.java
@@ -5,7 +5,12 @@ package de.tuberlin.cit.services;
  */
 
 import de.tuberlin.cit.services.interfaces.Hostservice;
+import org.apache.flink.runtime.instance.HardwareDescription;
+import org.apache.flink.runtime.instance.Instance;
+import org.apache.flink.runtime.instance.InstanceConnectionInfo;
+import org.apache.flink.runtime.instance.InstanceID;
 
+import java.net.*;
 import java.rmi.*;
 import java.rmi.server.*;
 
@@ -14,8 +19,36 @@ public class ServiceHost extends UnicastRemoteObject implements Hostservice {
     public ServiceHost () throws RemoteException {
 
     }
+
     @Override
-    public String getGoodHost() throws RemoteException {
-        return "This is a nice host to work with!";
+    public Instance getExecutionHost() throws RemoteException {
+        int dataPort = 1337;
+        int ipcPort = 1338;
+        String hostname = "127.0.0.1";
+
+        InstanceConnectionInfo conInfo = null;
+        try {
+            conInfo = new InstanceConnectionInfo(InetAddress.getByName(hostname), ipcPort, dataPort);
+        } catch (java.net.UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        int cpuCores = 2;
+        long physMem = 4096;
+        long jvmHeap = 1024;
+        long managedMem = 1024;
+
+        HardwareDescription hwd = new HardwareDescription(cpuCores,physMem,jvmHeap,managedMem);
+
+        InstanceID iId = new InstanceID();
+        int slots = 1;
+        Instance instance = new Instance(conInfo, iId, hwd, slots);
+
+        return instance;
+    }
+
+    @Override
+    public boolean canConnect() throws RemoteException {
+        return true;
     }
 }
--- sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/services/interfaces/Hostservice.java
+++ sdn-data-process-engine-middleware/src/main/java/de/tuberlin/cit/services/interfaces/Hostservice.java
@@ -4,10 +4,14 @@ package de.tuberlin.cit.services.interfaces;
  * Created by Nico on 07.01.2015.
  */
 
+import org.apache.flink.runtime.instance.Instance;
+
 import java.rmi.*;
 
 public interface Hostservice extends Remote{
 
-    public String getGoodHost() throws RemoteException;
+    public Instance getExecutionHost() throws RemoteException;
 
+    public boolean canConnect() throws RemoteException;
 }
+
