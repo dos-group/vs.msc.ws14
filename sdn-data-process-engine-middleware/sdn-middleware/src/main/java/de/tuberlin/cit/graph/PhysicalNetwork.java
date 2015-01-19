a4c59d89d889083de1428dd3cb7ee887d01a410a
Maxim Tschumak <maxim.tschumak@gmail.com>
2015-01-19 10:31:55 +0100

    added calculation of a distance matrix

--- sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/graph/PhysicalNetwork.java
+++ sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/graph/PhysicalNetwork.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class PhysicalNetwork {
     private static PhysicalNetwork instance;
+    private long[][] distances;
 
     private DirectedSparseMultigraph graph;
 
@@ -24,16 +25,38 @@ public class PhysicalNetwork {
         return instance;
     }
 
-    public void createOrUpdateGraph(final List<NetworkEdge> edges) {
+    public void createOrUpdateGraph(final List<NetworkEdge> edges, final List<NetworkVertex> vertices) {
+        this.graph = createGraph(edges);
+        this.distances = calculateDistanceMatrix(edges, vertices);
+    }
+
+    private long[][] calculateDistanceMatrix(final List<NetworkEdge> edges, final List<NetworkVertex> vertices){
+        int numberOfVertices = vertices.size();
+        long[][] distances = new long[numberOfVertices][numberOfVertices];
+        for (int indexVertexFrom = 0; indexVertexFrom<vertices.size(); indexVertexFrom++){
+            for (int indexVertexTo = 0; indexVertexTo<vertices.size(); indexVertexTo++) {
+                if (vertices.get(indexVertexFrom).equals(vertices.get(indexVertexTo))) {
+                    distances[indexVertexFrom][indexVertexTo] = 0;
+                } else {
+                    Number distance = getDistanceOfTheShortestPath(vertices.get(indexVertexFrom), vertices.get(indexVertexTo), 1);
+                    distances[indexVertexFrom][indexVertexTo] = distance.longValue();
+                }
+            }
+        }
+        return distances;
+    }
+
+    private DirectedSparseMultigraph<NetworkVertex, NetworkEdge> createGraph(List<NetworkEdge> edges){
         final DirectedSparseMultigraph<NetworkVertex, NetworkEdge> directedGraph = new DirectedSparseMultigraph();
         for (NetworkEdge edge : edges) {
             directedGraph.addEdge(edge, edge.getTailVertex(), edge.getHeadVertex(), EdgeType.DIRECTED);
         }
-        this.graph = directedGraph;
+        return directedGraph;
     }
 
-    public Number getLengthIfTheShortestPath(final NetworkVertex node1, final NetworkVertex node2, long sizeOfDataToTransport){
-        Transformer transformer = StaticGraphUtils.createTransporter(sizeOfDataToTransport);
+
+    private Number getDistanceOfTheShortestPath(final NetworkVertex node1, final NetworkVertex node2, long sizeOfDataToTransport){
+        Transformer transformer = StaticGraphUtils.createSimpleTransporter();
         DijkstraShortestPath<NetworkVertex,NetworkEdge> alg = new DijkstraShortestPath(this.graph, transformer);
         return alg.getDistance(node1,node2);
     }
