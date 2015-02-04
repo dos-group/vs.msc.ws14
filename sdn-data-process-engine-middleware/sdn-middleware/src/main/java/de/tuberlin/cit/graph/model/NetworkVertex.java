071e8be5509bc99fc4aec985d39247f7ecde3b58
Robert Oberbeck <rob-oberbeck@users.noreply.github.com>
2015-02-04 11:19:22 +0100

    removed unused GraphElement

--- sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/graph/model/NetworkVertex.java
+++ sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/graph/model/NetworkVertex.java
@@ -1,8 +1,30 @@
 package de.tuberlin.cit.graph.model;
 
-public class NetworkVertex extends GraphElement {
+public class NetworkVertex {
+    private String id;
 
     public NetworkVertex(String id) {
-        super(id);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NetworkVertex that = (NetworkVertex) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
