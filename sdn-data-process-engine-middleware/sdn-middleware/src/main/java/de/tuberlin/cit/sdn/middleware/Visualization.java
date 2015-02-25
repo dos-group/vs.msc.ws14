90584882a91af93faf259f2708b3f2be89c5be31
Robert Oberbeck <rob-oberbeck@users.noreply.github.com>
2015-02-25 10:28:32 +0100

    added visualizeNetworkGraph to StaticGraphUtils

--- sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/sdn/middleware/Visualization.java
+++ sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/sdn/middleware/Visualization.java
@@ -2,6 +2,7 @@ package de.tuberlin.cit.sdn.middleware;
 
 import de.tuberlin.cit.sdn.middleware.graph.NetworkFactory;
 import de.tuberlin.cit.sdn.middleware.graph.PhysicalNetwork;
+import de.tuberlin.cit.sdn.middleware.graph.StaticGraphUtils;
 import de.tuberlin.cit.sdn.middleware.graph.model.Host;
 import de.tuberlin.cit.sdn.middleware.graph.model.NetworkEdge;
 import de.tuberlin.cit.sdn.middleware.graph.model.NetworkVertex;
@@ -44,34 +45,7 @@ public class Visualization {
         network.createOrUpdateGraph(networkFactory.createNetworkEdges(topologies.get(0)));
 
         Graph<NetworkVertex, NetworkEdge> graph = network.getGraph();
-
-        Layout<NetworkVertex, NetworkEdge> layout = new ISOMLayout<>(graph);
-
-        layout.setSize(new Dimension(600, 600));
-        BasicVisualizationServer<NetworkVertex, NetworkEdge> vv = new BasicVisualizationServer<>(layout);
-        vv.setPreferredSize(new Dimension(700, 700));
-
-        Transformer<NetworkVertex, Paint> vertexPaint = v -> {
-            if (v instanceof Host) {
-                Host host = (Host) v;
-                return host.isFree() ? Color.GREEN : Color.RED;
-            } else {
-                return Color.GRAY;
-            }
-        };
-        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
-
-        Transformer<NetworkVertex, String> vertexLabel = v -> v.getId();
-        vv.getRenderContext().setVertexLabelTransformer(vertexLabel);
-        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.AUTO);
-
-        Transformer<NetworkEdge, String> edgeLabel = networkEdge -> networkEdge.getTailPort() + "->" + networkEdge.getHeadPort();
-        vv.getRenderContext().setEdgeLabelTransformer(edgeLabel);
-
-        JFrame frame = new JFrame("Simple Graph View 2");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(vv);
-        frame.pack();
+        JFrame frame = StaticGraphUtils.visualizeNetworkGraph(graph);
         frame.setVisible(true);
     }
 }
--- sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/sdn/middleware/graph/StaticGraphUtils.java
+++ sdn-data-process-engine-middleware/sdn-middleware/src/main/java/de/tuberlin/cit/sdn/middleware/graph/StaticGraphUtils.java
@@ -1,8 +1,17 @@
 package de.tuberlin.cit.sdn.middleware.graph;
 
 
+import de.tuberlin.cit.sdn.middleware.graph.model.Host;
 import de.tuberlin.cit.sdn.middleware.graph.model.NetworkEdge;
+import de.tuberlin.cit.sdn.middleware.graph.model.NetworkVertex;
+import edu.uci.ics.jung.algorithms.layout.ISOMLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import org.apache.commons.collections15.Transformer;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class StaticGraphUtils {
 
@@ -20,5 +29,36 @@ public class StaticGraphUtils {
                 return link.calculateWeight();
             }
         };
+    }
+
+    public static JFrame visualizeNetworkGraph(Graph<NetworkVertex, NetworkEdge> graph) {
+        Layout<NetworkVertex, NetworkEdge> layout = new ISOMLayout<>(graph);
+
+        layout.setSize(new Dimension(600, 600));
+        BasicVisualizationServer<NetworkVertex, NetworkEdge> vv = new BasicVisualizationServer<>(layout);
+        vv.setPreferredSize(new Dimension(700, 700));
+
+        Transformer<NetworkVertex, Paint> vertexPaint = v -> {
+            if (v instanceof Host) {
+                Host host = (Host) v;
+                return host.isFree() ? Color.GREEN : Color.RED;
+            } else {
+                return Color.GRAY;
+            }
+        };
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+
+        Transformer<NetworkVertex, String> vertexLabel = v -> v.getId();
+        vv.getRenderContext().setVertexLabelTransformer(vertexLabel);
+        vv.getRenderer().getVertexLabelRenderer().setPosition(edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position.AUTO);
+
+        Transformer<NetworkEdge, String> edgeLabel = networkEdge -> networkEdge.getTailPort() + "->" + networkEdge.getHeadPort();
+        vv.getRenderContext().setEdgeLabelTransformer(edgeLabel);
+
+        JFrame frame = new JFrame("Simple Graph View 2");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(vv);
+        frame.pack();
+        return frame;
     }
 }
